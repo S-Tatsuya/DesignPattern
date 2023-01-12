c01f5102b2c40ceeff1e86bc2f25042f493366e2
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-12 18:55:36 +0900

    refactor: pytest fixtureを活用

--- Pattern/Template/HeadFirstPython/tests/test_coffee.py
+++ Pattern/Template/HeadFirstPython/tests/test_coffee.py
@@ -1,31 +1,28 @@
+import pytest
+
 from src.Coffee import Coffee
 
 
 class TestCoffee:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.sut = Coffee()
+
     def test_prepare_recipe(self, capfd):
-        sut = Coffee()
-        sut.prepare_recipe()
+        self.sut.prepare_recipe()
 
         out, err = capfd.readouterr()
         assert out == "お湯を沸かします\nフィルタでコーヒーをドリップします\nカップに注ぎます\n砂糖とミルクを追加します\n"
         assert err == ""
 
     def test_coffee_boil_water(self):
-        sut = Coffee()
-
-        assert sut.boil_water() == "お湯を沸かします"
+        assert self.sut.boil_water() == "お湯を沸かします"
 
     def test_coffee_brew_coffee_grinds(self):
-        sut = Coffee()
-
-        assert sut.brew_coffee_grinds() == "フィルタでコーヒーをドリップします"
+        assert self.sut.brew_coffee_grinds() == "フィルタでコーヒーをドリップします"
 
     def test_coffee_pour_in_cup(self):
-        sut = Coffee()
-
-        assert sut.pour_in_cup() == "カップに注ぎます"
+        assert self.sut.pour_in_cup() == "カップに注ぎます"
 
     def test_coffee_add_sugar_and_milk(self):
-        sut = Coffee()
-
-        assert sut.add_sugar_and_milk() == "砂糖とミルクを追加します"
+        assert self.sut.add_sugar_and_milk() == "砂糖とミルクを追加します"
--- Pattern/Template/HeadFirstPython/tests/test_tee.py
+++ Pattern/Template/HeadFirstPython/tests/test_tee.py
@@ -1,31 +1,28 @@
+import pytest
+
 from src.Tea import Tea
 
 
 class TestTea:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.sut = Tea()
+
     def test_prepare_recipe(self, capfd):
-        sut = Tea()
-        sut.prepare_recipe()
+        self.sut.prepare_recipe()
 
         out, err = capfd.readouterr()
         assert out == "お湯を沸かします\n紅茶を浸します\nレモンを追加します\nカップに注ぎます\n"
         assert err == ""
 
     def test_tee_boil_water(self):
-        sut = Tea()
-
-        assert sut.boil_water() == "お湯を沸かします"
+        assert self.sut.boil_water() == "お湯を沸かします"
 
     def test_tee_steep_tea_bag(self):
-        sut = Tea()
-
-        assert sut.steep_tea_bag() == "紅茶を浸します"
+        assert self.sut.steep_tea_bag() == "紅茶を浸します"
 
     def test_tee_add_lemon(self):
-        sut = Tea()
-
-        assert sut.add_lemon() == "レモンを追加します"
+        assert self.sut.add_lemon() == "レモンを追加します"
 
     def test_tee_pour_in_cup(self):
-        sut = Tea()
-
-        assert sut.pour_in_cup() == "カップに注ぎます"
+        assert self.sut.pour_in_cup() == "カップに注ぎます"
