f911c227631dddf8600e32831aba031e3f5a01bf
S.tatsuya <s.tatsuya0123@gmail.com>
2023-01-13 11:08:06 +0900

    add: Duckクラスをソートするアルゴリズムを実装

--- Pattern/Template/HeadFirstPython/src/Duck.py
+++ Pattern/Template/HeadFirstPython/src/Duck.py
@@ -10,3 +10,27 @@ class Duck:
     @property
     def wight(self):
         return self._wight
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+
+        return self.wight == other.wight
+
+    def __lt__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplementedError()
+
+        return self.wight < other.wight
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other):
+        return not self.__lt__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
--- Pattern/Template/HeadFirstPython/tests/test_duck.py
+++ Pattern/Template/HeadFirstPython/tests/test_duck.py
@@ -2,8 +2,41 @@ from src.Duck import Duck
 
 
 class TestDuck:
-    def test_duck_wight(self):
+    def test_wight_and_name(self):
         sut = Duck("duck1", 1)
 
         assert sut.wight == 1
         assert sut.name == "duck1"
+
+    def test_compare(self):
+        sut_large = Duck("duck_large", 5)
+        sut_large2 = Duck("duck_large2", 5)
+        sut_small = Duck("duck_small", 1)
+
+        assert sut_large > sut_small
+        assert sut_small < sut_large
+        assert sut_large == sut_large2
+        assert sut_large != sut_small
+        assert sut_large >= sut_small
+        assert sut_small <= sut_large
+
+    def test_duck_sort(self):
+        suts = [
+            Duck("duck5", 5),
+            Duck("duck1", 1),
+            Duck("duck7", 7),
+            Duck("duck4", 4),
+            Duck("duck2", 2),
+            Duck("duck5", 5),
+        ]
+
+        result = sorted(suts)
+
+        assert result == [
+            Duck("duck1", 1),
+            Duck("duck2", 2),
+            Duck("duck4", 4),
+            Duck("duck5", 5),
+            Duck("duck5", 5),
+            Duck("duck7", 7),
+        ]
